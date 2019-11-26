696771f9828c8108ac708b3f353a6fa45bcb9a10
Bonnie Lin <b2lin@ucsd.edu>
2019-11-26 11:15:27 -0800

    transfer key details frm overview docs in dropbox; create the latin square with the order of these sessions

---
+++ CREATE/CREATE_RAW_MITCHELL.R
@@ -0,0 +1,30 @@
+### Extract and process RAW TEXT and CSV files
+
+#########################################
+########## DISCOUNTING ##################
+#########################################
+# Shipment1 Latin Square (From U01_Shipment1_LS_Data_Files_Overview.pdf)
+# Exceptions: 
+# 46260ismissing a delay 2 sessionintended for 2/11/19,due to amacro error theanimal was run on delay4 instead of 2and the session was not made up prior tothe animalbeing euthanized
+# 46266 is missing a delay 2 sessionintended for2/22/19. Due to a macro error,the animal was run on delay8instead ofdelay2and the session was not made up prior tothe animalbeing euthanized
+# Expectations: 89 subjects; provided 3202/3204 sessions for analysis
+
+# Shipment2 Latin Square (From U01_Shipment2_LS_Data_Files_Overview.pdf)
+# Expectations: 110 subjects; provided 3960/3960 sessions for analysis
+
+# Each subject performed 6 iterations of each time delay 0s,2s, 4s, 8s, 16s, and 24sin the delay discounting task(89X36 sessions)
+discount_latinsquare_order <- c(4, 8, 2, 16, 0, 24,
+                                 2, 4, 0, 8, 24, 16,
+                                 8, 16, 4, 24, 2, 0,
+                                 24, 0 ,16, 2, 8, 4,
+                                 16, 24, 8, 0, 4, 2,
+                                 0, 2, 24, 4, 16, 8) 
+
+
+
+
+
+
+#########################################
+############ LOCOMOTOR ##################
+#########################################
