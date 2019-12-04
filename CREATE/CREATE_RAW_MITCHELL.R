7a1eea8c9a7d47d1109b032c4800fa86948116d9
Bonnie Lin <b2lin@ucsd.edu>
2019-12-04 11:12:10 -0800

    save last sessions changes

--- CREATE/CREATE_RAW_MITCHELL.R
+++ CREATE/CREATE_RAW_MITCHELL.R
@@ -11,7 +11,7 @@
 
 # Shipment2 Latin Square (From U01_Shipment2_LS_Data_Files_Overview.pdf)
 # Expectations: 110 subjects; provided 3960/3960 sessions for analysis
-
+  
 # Each subject performed 6 iterations of each time delay 0s,2s, 4s, 8s, 16s, and 24sin the delay discounting task(89X36 sessions)
 discount_latinsquare_order <- c(4, 8, 2, 16, 0, 24,
                                  2, 4, 0, 8, 24, 16,
@@ -23,6 +23,9 @@ discount_latinsquare_order <- c(4, 8, 2, 16, 0, 24,
 
 
 
+##  Test sessions last: 10 minutes or cumulative total 5 ml of water consumption
+
+# The first and last patch changes of the test session were excluded 
 
 
 #########################################
