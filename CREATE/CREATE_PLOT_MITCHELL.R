7e8c90ba723b05301ef952f2b42d42cf2a023e8b
Bonnie Lin <b2lin@ucsd.edu>
2020-01-22 15:04:15 -0800

    rerun codes to create database update

--- CREATE/CREATE_PLOT_MITCHELL.R
+++ CREATE/CREATE_PLOT_MITCHELL.R
@@ -16,6 +16,8 @@ discountingvalidtraits_graph <- discountingvalidtraits %>%
 discountingtraits_extract <- data.frame(var_abv = grep(pattern = "_", names(discountingvalidtraits_graph), perl = T, value = T) %>% as.character(),
                                     var_graph = c("total number of trials", "number of free choice trials", "number of forced delay trials", "number of forced immediate trials",
                                                   "number of forced trials", "number of free choice events before center nose pose", "number of forced delay events before center",
+                                                  "average reaction times from start to center on the delayed side", "average reaction times from center to choice on the delayed side",
+                                                  "average choice reaction times from start to center on the immediate side", "average choice reaction times from center to choice on the immediate side", 
                                                   "experiment age")) %>% 
   mutate(var_abv = unlist(var_abv) %>% as.character,
          var_graph = unlist(var_graph) %>% as.character)
@@ -28,27 +30,30 @@ plot_compare_list = list()
 
 for (i in seq_along(discountingtraits_extract$var_abv)){
 
-  plot_by_cohort <- ggplot(discountingvalidtraits_graph, aes(x = cohort, group = cohort)) +
-    geom_boxplot(aes_string(y = discountingtraits_extract$var_abv[i]), outlier.size = 0.75) +
-    geom_jitter(aes_string(y = discountingtraits_extract$var_abv[i]), alpha = 0.3, position=position_jitter(0.2), size = 0.5) +
-    labs(title = paste0(toupper(discountingtraits_extract$var_graph[i]), "_Discounting_U01_Mitchell", "\n"), x = "Cohort", fill = "Cohort") +  # scale_fill_discrete(name = "New Legend Title")
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  # plot_by_cohort <- ggplot(discountingvalidtraits_graph, aes(x = cohort, group = cohort)) +
+  #   geom_boxplot(aes_string(y = discountingtraits_extract$var_abv[i]), outlier.size = 0.75) +
+  #   geom_jitter(aes_string(y = discountingtraits_extract$var_abv[i]), alpha = 0.3, position=position_jitter(0.2), size = 0.5) +
+  #   labs(title = paste0(toupper(discountingtraits_extract$var_graph[i]), "_Discounting_U01_Mitchell", "\n"), x = "Cohort", fill = "Cohort") +  # scale_fill_discrete(name = "New Legend Title")
+  #   theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
-  plot_by_box <- ggplot(discountingvalidtraits_graph, aes(x = as.factor(box), group = as.factor(box))) +
-    geom_boxplot(aes_string(y = discountingtraits_extract$var_abv[i]), outlier.size = 0.75) +
-    geom_jitter(aes_string(y = discountingtraits_extract$var_abv[i]), alpha = 0.3, position=position_jitter(0.2), size = 0.5) +
-    labs(title = paste0(toupper(discountingtraits_extract$var_graph[i]), "_Discounting_U01_Mitchell", "\n"), x = "Box", fill = "Box") +  # scale_fill_discrete(name = "New Legend Title")
-    theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  # plot_by_box <- ggplot(discountingvalidtraits_graph, aes(x = as.factor(box), group = as.factor(box))) +
+  #   geom_boxplot(aes_string(y = discountingtraits_extract$var_abv[i]), outlier.size = 0.75) +
+  #   geom_jitter(aes_string(y = discountingtraits_extract$var_abv[i]), alpha = 0.3, position=position_jitter(0.2), size = 0.5) +
+  #   labs(title = paste0(toupper(discountingtraits_extract$var_graph[i]), "_Discounting_U01_Mitchell", "\n"), x = "Box", fill = "Box") +  # scale_fill_discrete(name = "New Legend Title")
+  #   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
   plot_by_sex <- ggplot(discountingvalidtraits_graph, aes(x = as.factor(sex), group = as.factor(sex))) +
     geom_boxplot(aes_string(y = discountingtraits_extract$var_abv[i]), outlier.size = 0.75) +
     geom_jitter(aes_string(y = discountingtraits_extract$var_abv[i]), alpha = 0.3, position=position_jitter(0.2), size = 0.5) +
-    labs(title = paste0(toupper(discountingtraits_extract$var_graph[i]), "_Discounting_U01_Mitchell", "\n"), x = "Sex", fill = "Sex") +  # scale_fill_discrete(name = "New Legend Title")
+    labs(title = stringr::str_wrap(paste0(toupper(discountingtraits_extract$var_graph[i]), 
+                                 "_Discounting_U01_Mitchell", "\n"), width = 100), 
+                          x = "Sex", 
+                          fill = "Sex") +  # scale_fill_discrete(name = "New Legend Title")
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
   
 
-  print(plot_by_cohort)
-  print(plot_by_box)
+  # print(plot_by_cohort)
+  # print(plot_by_box)
   print(plot_by_sex)
   
   
--- CREATE/CREATE_RAW_MITCHELL.R
+++ CREATE/CREATE_RAW_MITCHELL.R
@@ -1,4 +1,7 @@
 ### Extract and process RAW TEXT and CSV files
+# remove all but one object from the global environment
+# rm(list=setdiff(ls(), "WFU_Mitchell_test_df"))
+
 
 library(tidyr)
 library(lubridate)
@@ -16,12 +19,12 @@ library(lubridate)
 # Expectations: 110 subjects; provided 3960/3960 sessions for analysis
 
 # Each subject performed 6 iterations of each time delay 0s,2s, 4s, 8s, 16s, and 24sin the delay discounting task(89X36 sessions)
-discount_latinsquare_order <- c(4, 8, 2, 16, 0, 24,
-                                2, 4, 0, 8, 24, 16,
-                                8, 16, 4, 24, 2, 0,
-                                24, 0 ,16, 2, 8, 4,
-                                16, 24, 8, 0, 4, 2,
-                                0, 2, 24, 4, 16, 8) 
+# discount_latinsquare_order <- c(4, 8, 2, 16, 0, 24,
+#                                 2, 4, 0, 8, 24, 16,
+#                                 8, 16, 4, 24, 2, 0,
+#                                 24, 0 ,16, 2, 8, 4,
+#                                 16, 24, 8, 0, 4, 2,
+#                                 0, 2, 24, 4, 16, 8) 
 # include code to unzip the files and duplicate the files from the original directory to the new one
 #### XXXXXXXXXXXXx
 
@@ -86,40 +89,41 @@ discounting_filenames <- list.files(path = ".", pattern = "*.txt", recursive = T
 
 # ideas: get the start of the session + number in c array
 # 
-# 
-readCelems<- function(x){
-  C_elems <- fread(paste0("sed -n '22p' ", "'", x, "'")) 
-  C_elems$filename <- x
-  return(C_elems)
-}
-readCelems_df <- lapply(discounting_filenames[1:10],readCelems) %>% rbindlist() 
-readCelems_df %>% 
-  dplyr::rename("Date_Time" = "V1") %>% 
-  group_by(filename) %>% 
-  dplyr::mutate(row_number = 1:n()) %>% head
+# MIGHT DELETE SINCE I DIDN'T END UP USING THIS APPROACH
+# readCelems<- function(x){
+#   C_elems <- fread(paste0("sed -n '22p' ", "'", x, "'")) 
+#   C_elems$filename <- x
+#   return(C_elems)
+# }
+# readCelems_df <- lapply(discounting_filenames[1:10],readCelems) %>% rbindlist() 
+# readCelems_df %>% 
+#   dplyr::rename("Date_Time" = "V1") %>% 
+#   group_by(filename) %>% 
+#   dplyr::mutate(row_number = 1:n()) %>% head
 
+#only run if needed
 # checking if date time in file matches date time in filename
-readDateTimes <- function(x){
-  Date_Time <- fread(paste0("sed -n '1, 3p; 11, 12p' ", "'", x, "'"))
-  Date_Time$filename <- x
-  return(Date_Time)
-}
-
-readDateTimes_df <- lapply(discounting_filenames, readDateTimes) %>% rbindlist() %>% 
-  dplyr::rename("Date_Time" = "V1") %>% 
-  group_by(filename) %>% 
-  dplyr::mutate(row_number = row_number()) %>% 
-  spread(row_number, Date_Time) %>% 
-  unite("date", "1":"3", sep = "/") %>% 
-  unite("time", "4":"5", sep = ":") %>%  
-  unite("datetime", "date":"time", sep = " ") %>% 
-  mutate(datetime = lubridate::mdy_hm(datetime, tz = "UTC"),
-         datetime_file = stringr::str_match(filename, "square/(.*?)m_")[,2] %>% 
-           mgsub(., c("_", "h"), c(" ", ":")) %>% 
-           lubridate::ymd_hm(., tz = "UTC")) #removed the 11,13p in sed statemetn and time 4:6 to account for the missing second information from filename
-
-readDateTimes_df %>% 
-  dplyr::filter(datetime != datetime_file) %>% dim # 0 no cases; all match 
+# readDateTimes <- function(x){
+#   Date_Time <- fread(paste0("sed -n '1, 3p; 11, 12p' ", "'", x, "'"))
+#   Date_Time$filename <- x
+#   return(Date_Time)
+# }
+# 
+# readDateTimes_df <- lapply(discounting_filenames, readDateTimes) %>% rbindlist() %>% 
+#   dplyr::rename("Date_Time" = "V1") %>% 
+#   group_by(filename) %>% 
+#   dplyr::mutate(row_number = row_number()) %>% 
+#   spread(row_number, Date_Time) %>% 
+#   unite("date", "1":"3", sep = "/") %>% 
+#   unite("time", "4":"5", sep = ":") %>%  
+#   unite("datetime", "date":"time", sep = " ") %>% 
+#   mutate(datetime = lubridate::mdy_hm(datetime, tz = "UTC"),
+#          datetime_file = stringr::str_match(filename, "square/(.*?)m_")[,2] %>% 
+#            mgsub(., c("_", "h"), c(" ", ":")) %>% 
+#            lubridate::ymd_hm(., tz = "UTC")) #removed the 11,13p in sed statemetn and time 4:6 to account for the missing second information from filename
+# 
+# readDateTimes_df %>% 
+#   dplyr::filter(datetime != datetime_file) %>% dim # 0 no cases; all match 
 
 # extract delay information to be joined by filename
 readdelay_discounting <- function(x){
@@ -137,20 +141,28 @@ readdiscounting <- function(x){
   # C_elems <- fread(paste0("sed -n '22p' ", "'", x, "'")) 
   
   discounting <- fread(paste0("sed -n /-100/,/30000/p ", "'", x, "'"))
-  discounting <- discounting[-nrow(discounting),]
+  if(discounting[nrow(discounting),] == 30000){
+    discounting <- discounting[-nrow(discounting),]
+  }  # 1/10 "The session should end at the 360000-ish mark so no problem having things in the 300000s" - Suzanne Mitchell
   indices <- which(discounting$V1 < 0)
   discounting_split <- split(discounting, cumsum(1:nrow(discounting) %in% indices))
   
   discounting_split_newcols <- lapply(discounting_split, function(x){
     names(x) <- "codes"
-    x$timefromstart <- tail(x$codes, 1)
-    x <- x[-nrow(x),]
     
-    if(nrow(x) > 1){
+    if(nrow(x) == 2){
+      x$timefromstart <- tail(x$codes, 1)
+      x <- x[-nrow(x),]
+    }
+    
+    
+    if(nrow(x) > 2){
+      x$timefromstart <- x$codes[4]
       x$reward <- x[2, 1]
       x$adjustingamt <- x[3, 1]
       x <- x[1,]
     }
+    
     
     return(x)
   }) %>% rbindlist(fill = T)
@@ -162,14 +174,17 @@ readdiscounting <- function(x){
 }
 
 discounting_df <- lapply(discounting_filenames, readdiscounting) %>% rbindlist(fill = T) # 7162 files
-# summary looks good, no positive numbers in codes and all positive times
+# summary looks good, no positive numbers in codes and almost all positive timestamps (only one na in file == "./Ship1_Latin-square/2019-03-22_15h05m_Subject 45883.txt")
 
 discounting_df_expanded <- discounting_df %>% 
   dplyr::rename('filename' = 'file') %>% 
-  mutate(subject = str_match(filename, "Subject (.*?)\\.txt")[,2],
-         date = str_extract(filename, "\\d{4}-\\d{2}-\\d{2}"),
-         time = gsub("h", ":", str_extract(filename, "\\d{2}h\\d{2}")),
-         date = as.POSIXct(date))
+  dplyr::mutate(subject = str_match(filename, "Subject (.*?)\\.txt")[,2],
+                date = str_extract(filename, "\\d{4}-\\d{2}-\\d{2}"),
+                time = gsub("h", ":", str_extract(filename, "\\d{2}h\\d{2}")),
+                date = as.POSIXct(date)) %>% 
+  dplyr::arrange(subject, date) %>% 
+  dplyr::group_by(filename) %>% 
+  dplyr::mutate(rep = dplyr::row_number())
 
 # extract squad (?) and box information
 readsquadbox <- function(x){
@@ -261,7 +276,7 @@ eventchoices <- data.frame("key" = c(-100, -1, -3, -5, -6, -7),
                            "meaning" = c("begin_trial", "leverpress_imm", "leverpress_del", "nosepoke_imm", "nosepoke_center", "nosepoke_del"))
 eventchoices_full <- eventchoices %>% 
   rbind(data.frame("key" = eventchoices$key[2:6] - 10, 
-                                  "meaning" = paste0(eventchoices$meaning[2:6], "_choice"))) %>% 
+                   "meaning" = paste0(eventchoices$meaning[2:6], "_choice"))) %>% 
   rbind(data.frame("key" = -20, "meaning" = "begin_to")) %>% 
   rbind(data.frame("key" = eventchoices$key[2:6] - 20, 
                    "meaning" = paste0(eventchoices$meaning[2:6], "_to"))) %>% 
@@ -270,8 +285,8 @@ eventchoices_full <- eventchoices %>%
                    "meaning" = paste0(.$meaning[2:11], "_fd"))) %>% 
   rbind(data.frame("key" = .$key[2:11] - 200, 
                    "meaning" = paste0(.$meaning[2:11], "_fi")))
-  
-  
+
+
 # subset_disc %>% 
 #     group_by(filename) %>% 
 #     summarize(subject = head(subject, 1),
@@ -321,8 +336,8 @@ discountingvalidtraits <- discounting_df_expanded %>%
             events_before_center = length(timefromstart[codes %in% c("-1", "-3", "-5", "-7")]),
             
             avg_rxn_time = mean(timefromstart[codes %in% c("-6")])
-            )
-            
+  )
+
 subset_disc%>% 
   group_by(filename) %>% 
   
@@ -342,35 +357,22 @@ subset_disc%>%
             
             # avg_rxn_time = mean(timefromstart[codes %in% c("-6")]),
             
-  )
+  ) %>% 
+  left_join(., rxntime_crt_avg[c("avg_rxn_time_del", "avg_choice_rxn_time_del", "avg_rxn_time_imm", "avg_choice_rxn_imm", "filename")], by = filename)
 
 subset_disc%>% 
   group_by(filename) %>% 
-
-## finding choice_reaction_time
-choice_reaction_time <- ddply(subset_disc, .(filename), transform, choicereactiontime=timefromstart-timefromstart[codes == -100])
-subset_disc %>% dplyr::filter(codes == -6|!is.na(reward))
   
+  ## finding choice_reaction_time
+  choice_reaction_time <- ddply(subset_disc, .(filename), transform, choicereactiontime=timefromstart-timefromstart[codes == -100])
+subset_disc %>% dplyr::filter(codes == -6|!is.na(reward))
+
 ## exploration  -- make sense of the data
 ## discounting_df_expanded %>% subset(codes %in% c(-6, -13) & subject == "46067" & delay == 4 & date == "2019-02-07")%>% distinct(adjustingamt, .keep_all = T) %>% View(.)
 ## discounting_df_expanded %>% subset(!is.na(adjustingamt) & subject == "46067" & delay == 4 & date == "2019-02-07")%>% distinct(adjustingamt, .keep_all = T) %>% View(.)
 
-reactiontimes <- discounting_df_expanded %>% subset(codes %in% c(-100, -13, -6))
+reactiontimes <- discounting_df_expanded %>% subset(codes %in% c(-100, -13, -6, -11))
 rxn_time <- lapply(split(reactiontimes, cumsum(1:nrow(reactiontimes) %in% which(reactiontimes$codes == -100))), function(x){
-  x <- x %>% 
-    mutate(rxn_time = ifelse(nrow(x)==2, NA, x$timefromstart[2] - x$timefromstart[1]),
-           choice_time = ifelse(nrow(x)==2, NA, x$timefromstart[3] - x$timefromstart[2])) %>% 
-    slice(1) %>% 
-    dplyr::select(-one_of(c("codes", "timefromstart", "reward","adjustingamt")))
-  return(x) 
-  }) %>% rbindlist() %>% 
-  group_by(filename) %>% 
-  dplyr::mutate(trial_id = paste0("trial_", dplyr::row_number()),
-         rxn_time = rxn_time/100)
-
-
-subset_disc_onlyinterestedcodes <- subset_disc %>% subset(codes %in% c(-100, -13, -6, -11))
-test_rxntime_crt <- lapply(split(subset_disc_onlyinterestedcodes, cumsum(1:nrow(subset_disc_onlyinterestedcodes) %in% which(subset_disc_onlyinterestedcodes$codes == -100))), function(x){
   x <- x %>% 
     mutate(rxn_time_del = ifelse(x$codes[3] == -13, x$timefromstart[2] - x$timefromstart[1], NA),
            choice_rxn_time_del = ifelse(x$codes[3] == -13, x$timefromstart[3] - x$timefromstart[2], NA),
@@ -380,27 +382,60 @@ test_rxntime_crt <- lapply(split(subset_disc_onlyinterestedcodes, cumsum(1:nrow(
     dplyr::select(-one_of(c("codes", "timefromstart", "reward","adjustingamt")))
   return(x) 
 }) %>% rbindlist() %>% 
-  dplyr::group_by(filename) %>% 
-  # dplyr::mutate(trial_id = paste0("trial_", dplyr::row_number()),
-  dplyr::mutate(trial_id = dplyr::row_number(),
+  group_by(filename) %>% 
+  dplyr::mutate(trialid = dplyr::row_number(),
                 rxn_time_del = rxn_time_del/100,
-                # choice_rxn_time_del = choice_rxn_time_del/100,
                 rxn_time_imm = rxn_time_imm/100) %>% 
-# , choice_rxn_imm = choice_rxn_imm/100) %>% 
   ungroup() # create individual trial information
 
 # aggregate above individual trial information to get group values
-test_rxntime_crt_avg <- test_rxntime_crt %>% 
+rxntime_crt_avg <- rxn_time %>% 
   dplyr::group_by(filename) %>% 
   slice(31:n()) %>% 
   dplyr::mutate(avg_rxn_time_del = mean(rxn_time_del, na.rm = T),
-            avg_choice_rxn_time_del = mean(choice_rxn_time_del, na.rm = T),
-            avg_rxn_time_imm = mean(rxn_time_imm, na.rm = T), 
-            avg_choice_rxn_imm = mean(choice_rxn_imm, na.rm = T)) %>%
+                avg_choice_rxn_time_del = mean(choice_rxn_time_del, na.rm = T),
+                avg_rxn_time_imm = mean(rxn_time_imm, na.rm = T), 
+                avg_choice_rxn_imm = mean(choice_rxn_imm, na.rm = T)) %>%
   # mutate_at(vars(contains('avg')), as.numeric) %>% 
   dplyr::select(-c(rxn_time_del, choice_rxn_time_del, rxn_time_imm, choice_rxn_imm)) %>% 
-  dplyr::filter(trial_id == max(trial_id)) ## won't be correct if you load plyr after dplyr
+  dplyr::filter(trialid == max(trialid)) ## won't be correct if you load plyr after dplyr
 
+rxntime_crt_avg %>% subset(avg_choice_rxn_time_del < 0)
+
+
+## testing on three files, each for diff subject
+# subset_disc_onlyinterestedcodes <- subset_disc %>% subset(codes %in% c(-100, -13, -6, -11))
+# test_rxntime_crt <- lapply(split(subset_disc_onlyinterestedcodes, cumsum(1:nrow(subset_disc_onlyinterestedcodes) %in% which(subset_disc_onlyinterestedcodes$codes == -100))), function(x){
+#   x <- x %>% 
+#     mutate(rxn_time_del = ifelse(x$codes[3] == -13, x$timefromstart[2] - x$timefromstart[1], NA),
+#            choice_rxn_time_del = ifelse(x$codes[3] == -13, x$timefromstart[3] - x$timefromstart[2], NA),
+#            rxn_time_imm = ifelse(x$codes[3] == -11, x$timefromstart[2] - x$timefromstart[1], NA),
+#            choice_rxn_imm = ifelse(x$codes[3] == -11, x$timefromstart[3] - x$timefromstart[2], NA)) %>% 
+#     slice(1) %>% 
+#     dplyr::select(-one_of(c("codes", "timefromstart", "reward","adjustingamt")))
+#   return(x) 
+# }) %>% rbindlist() %>% 
+#   dplyr::group_by(filename) %>% 
+#   # dplyr::mutate(trial_id = paste0("trial_", dplyr::row_number()),
+#   dplyr::mutate(trial_id = dplyr::row_number(),
+#                 rxn_time_del = rxn_time_del/100,
+#                 # choice_rxn_time_del = choice_rxn_time_del/100,
+#                 rxn_time_imm = rxn_time_imm/100) %>% 
+# # , choice_rxn_imm = choice_rxn_imm/100) %>% 
+#   ungroup() # create individual trial information
+# 
+# # aggregate above individual trial information to get group values
+# test_rxntime_crt_avg <- test_rxntime_crt %>% 
+#   dplyr::group_by(filename) %>% 
+#   slice(31:n()) %>% 
+#   dplyr::mutate(avg_rxn_time_del = mean(rxn_time_del, na.rm = T),
+#             avg_choice_rxn_time_del = mean(choice_rxn_time_del, na.rm = T),
+#             avg_rxn_time_imm = mean(rxn_time_imm, na.rm = T), 
+#             avg_choice_rxn_imm = mean(choice_rxn_imm, na.rm = T)) %>%
+#   # mutate_at(vars(contains('avg')), as.numeric) %>% 
+#   dplyr::select(-c(rxn_time_del, choice_rxn_time_del, rxn_time_imm, choice_rxn_imm)) %>% 
+#   dplyr::filter(trial_id == max(trial_id)) ## won't be correct if you load plyr after dplyr
+# 
 
 # helpful if sapply, but changed to lapply
 # %>% 
