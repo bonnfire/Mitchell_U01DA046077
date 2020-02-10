c64cd360ec60d067b3f0fe00e3b5246f17a5bcc7
Bonnie Lin <b2lin@ucsd.edu>
2020-02-10 14:35:22 -0800

    extract the metadata from the excels in the addl shipment info sheets

--- CREATE/EXTRACT_EXCEL.R
+++ CREATE/EXTRACT_EXCEL.R
@@ -4,3 +4,19 @@ setwd("~/Dropbox (Palmer Lab)/Suzanne_Mitchell_U01/Protocol-materials/DD-program
 
 # extract the shipment files from this directory
 shipmentfiles <- list.files(path = ".", pattern = "Shipment\\d.xlsm")
+
+
+
+## extract the excel metadata about boxes/squads
+setwd("~/Dropbox (Palmer Lab)/Suzanne_Mitchell_U01/U01_Shipment_Details_")
+metadatafiles <- list.files(path = ".", pattern = "*.xlsx")
+metadata <- lapply(metadatafiles, read_excel, sheet = "Sheet1")
+names(metadata) = str_pad(1:length(metadata), 2, "left", "0")
+metadata <- lapply(metadata, function(x){
+  x <- x %>% clean_names %>% 
+    rename("rfid" = "transponder_id",
+           "subject" = "last_5") %>% 
+    select(one_of("rfid", "subject", "box_color", "computer", "operant_box", "assigned_lever"))
+  return(x)
+})
+metadata <- metadata %>% rbindlist(idcol = "cohort")
