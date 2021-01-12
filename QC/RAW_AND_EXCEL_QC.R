2d2d36dbc6aecbc6b595fe0702ca0c9f927d5476
Bonnie Lin <b2lin@ucsd.edu>
2021-01-12 07:52:19 -0800

    update R object and db with age for delayed discounting

--- QC/RAW_AND_EXCEL_QC.R
+++ QC/RAW_AND_EXCEL_QC.R
@@ -92,14 +92,15 @@ locomotor_gwas_metadata_c01_05 <- locomotor_gwas %>%
 
 ## GWAS DELAYED DISCOUNTING (after suzanne post nida meeting)
 mitchell_c01_04_gwas_dd <- mitchell_c01_04_dd_xl_df %>% 
-  left_join(dd_metadata_c01_04, by = "rfid")
+  left_join(dd_metadata_c01_04, by = "rfid") %>% 
+  left_join(discounting_c01_04_age, by = c("rfid", "cohort"))
 
 # after checking that operant_box == dd_operant_box and computer is proxy for box_color
 mitchell_c01_04_gwas_dd <- mitchell_c01_04_gwas_dd %>% 
   rename("auc_trad" = "auc_traditional", 
          "auc_norm0" = "auc_normal0s",
          "hyperbolic_lnk" = "hyperbolic_ln_k") %>% 
-  select(cohort, rfid, sex, dd_squad_number, dd_computer, dd_operant_box, dd_assigned_lever, s_or_ns, hyperbolic_k, hyperbolic_lnk, hyperbolic_b, auc_trad, auc_norm0, quasi_h_k_beta, quasi_h_s_delta)
+  select(cohort, rfid, sex, dd_squad_number, dd_computer, dd_operant_box, dd_assigned_lever, s_or_ns, hyperbolic_k, hyperbolic_lnk, hyperbolic_b, auc_trad, auc_norm0, quasi_h_k_beta, quasi_h_s_delta, experimentage)
 
 
 
